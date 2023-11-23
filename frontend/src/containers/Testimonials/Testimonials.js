e7b6fb89b8e76a269569c3aa1888e9b71914c9ac
shivay <shivaysharma77894@gmail.com>
2023-11-24 00:36:23 +0530

    Bug Fix

--- frontend/src/containers/Testimonials/Testimonials.js
+++ frontend/src/containers/Testimonials/Testimonials.js
@@ -10,49 +10,70 @@ const Testimonial = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [testimonials, setTestimonials] = useState([]);
   const [brands, setBrands] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTestimonials = async () => {
+      try {
+        const query = '*[_type == "testimonials"]';
+        const brandsQuery = '*[_type == "brands"]';
+
+        const testimonialsData = await client.fetch(query);
+        const brandsData = await client.fetch(brandsQuery);
+
+        setTestimonials(testimonialsData);
+        setBrands(brandsData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTestimonials();
+  }, []);
 
   const handleClick = (index) => {
     setCurrentIndex(index);
   };
 
-  useEffect(() => {
-    const query = '*[_type == "testimonials"]';
-    const brandsQuery = '*[_type == "brands"]';
-
-    client.fetch(query).then((data) => {
-      setTestimonials(data);
-    });
-
-    client.fetch(brandsQuery).then((data) => {
-      setBrands(data);
-    });
-  }, []);
-
   return (
-    <>
-      {testimonials.length && (
+    <div className="app__testimonial">
+      {loading ? (
+        <p>Loading testimonials...</p>
+      ) : testimonials.length > 0 ? (
         <>
           <div className="app__testimonial-item app__flex">
-            <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
+            {testimonials[currentIndex]?.imageurl && (
+              <img src={urlFor(testimonials[currentIndex]?.imageurl)} alt={testimonials[currentIndex]?.name} />
+            )}
             <div className="app__testimonial-content">
-              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <p className="p-text">{testimonials[currentIndex]?.feedback}</p>
               <div>
-                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
-                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+                <h4 className="bold-text">{testimonials[currentIndex]?.name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex]?.company}</h5>
               </div>
             </div>
           </div>
 
           <div className="app__testimonial-btns app__flex">
-            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+            <div
+              className="app__flex"
+              onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}
+            >
               <HiChevronLeft />
             </div>
 
-            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+            <div
+              className="app__flex"
+              onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}
+            >
               <HiChevronRight />
             </div>
           </div>
         </>
+      ) : (
+        <p>No testimonials available.</p>
       )}
 
       <div className="app__testimonial-brands app__flex">
@@ -62,16 +83,12 @@ const Testimonial = () => {
             transition={{ duration: 0.5, type: 'tween' }}
             key={brand._id}
           >
-            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+            {brand?.imageurl && <img src={urlFor(brand?.imageurl)} alt={brand?.name} />}
           </motion.div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
-export default AppWrap(
-  MotionWrap(Testimonial, 'app__testimonial'),
-  'testimonial',
-  'app__primarybg',
-);
+export default AppWrap(MotionWrap(Testimonial, 'app__testimonial'), 'testimonial', 'app__primarybg');
